ed97894ce5a9d59bef3eb36c5db19a7754804aa6
jip11 <58308465+jip11@users.noreply.github.com>
2025-03-10 10:01:45 +0100

    foly

--- FrontEnd/src/views/HomePage.vue
+++ FrontEnd/src/views/HomePage.vue
@@ -33,7 +33,7 @@ export default {
     .container {
             position: relative;
             width: 100%;
-            height: 100%;
+            height: 800px;
             display: flex;
         }
     .container div{
